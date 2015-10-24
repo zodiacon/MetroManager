7c405e8168c936ca88e6558a0041c03ef635707a
zodiacon <zodiacon@live.com>
2015-10-24 11:37:21 +0300

    added version coluimn and double-click to launch

---
+++ MetroManager/Converters/VersionToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Windows.ApplicationModel;
+
+namespace MetroManager.Converters {
+	class VersionToStringConverter : IValueConverter {
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+			var version = (PackageVersion)value;
+			return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+			throw new NotImplementedException();
+		}
+	}
+}
