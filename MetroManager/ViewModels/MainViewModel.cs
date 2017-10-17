abb23d92c670da2fc4cd567a08cc627d2b4ab2e2
Pavel Yosifovich <zodiacon@live.com>
2017-10-17 10:37:44 +0300

    added search box

--- MetroManager/ViewModels/MainViewModel.cs
+++ MetroManager/ViewModels/MainViewModel.cs
@@ -5,37 +5,61 @@ using Prism.Mvvm;
 using Windows.ApplicationModel;
 using Windows.Management.Deployment;
 using System.Windows.Input;
+using System.Windows.Data;
+using System.Linq;
 
 namespace MetroManager.ViewModels {
-	class MainViewModel : BindableBase {
-		public IEnumerable<Package> Packages => new PackageManager().FindPackagesForUser(string.Empty);
+    class MainViewModel : BindableBase {
+        IEnumerable<Package> _packages;
+        public IEnumerable<Package> Packages => _packages ?? (_packages = new PackageManager().FindPackagesForUser(string.Empty).ToArray());
 
-		public DelegateCommand LaunchCommand { get; }
-
-
-		public MainViewModel() {
-			LaunchCommand = new DelegateCommand(
-				() => {
-					try {
-						MetroLauncher.LaunchApp(SelectedPackage.Id.FullName);
-					}
-					catch {
-						MessageBox.Show("Error launching app", "Metro Launcher");
-					}
-				},
-				() => SelectedPackage != null && !SelectedPackage.IsFramework)
-				.ObservesProperty(() => SelectedPackage);
-
-		}
-
-		private Package _selectedPackage;
-
-		public Package SelectedPackage {
-			get { return _selectedPackage; }
-			set { SetProperty(ref _selectedPackage, value);	}
-		}
+        public DelegateCommand LaunchCommand { get; }
 
 
-		public ICommand RefreshCommand => new DelegateCommand(() => OnPropertyChanged(nameof(Packages)));
-	}
+        public MainViewModel() {
+            LaunchCommand = new DelegateCommand(
+                () => {
+                    try {
+                        MetroLauncher.LaunchApp(SelectedPackage.Id.FullName);
+                    }
+                    catch {
+                        MessageBox.Show("Error launching app", "Metro Launcher");
+                    }
+                },
+                () => SelectedPackage != null && !SelectedPackage.IsFramework)
+                .ObservesProperty(() => SelectedPackage);
+
+        }
+
+        private Package _selectedPackage;
+
+        public Package SelectedPackage {
+            get { return _selectedPackage; }
+            set { SetProperty(ref _selectedPackage, value); }
+        }
+
+
+        public ICommand RefreshCommand => new DelegateCommand(() => OnPropertyChanged(nameof(Packages)));
+
+        private string _searchText;
+
+        public string SearchText {
+            get => _searchText;
+            set {
+                if (SetProperty(ref _searchText, value)) {
+                    var view = CollectionViewSource.GetDefaultView(Packages);
+                    if (string.IsNullOrEmpty(value))
+                        view.Filter = null;
+                    else {
+                        var text = value.ToLower();
+                        view.Filter = obj => {
+                            var package = (Package)obj;
+                            return package.Id.Name.ToLower().Contains(text);
+                        };
+                    }
+                }
+            }
+        }
+
+    }
 }
